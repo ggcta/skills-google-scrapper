db7c5f09e2959dabaef0ba5c0329c59e0a5e1f24
Sam Dinh <xuansam@google.com>
2025-04-25 13:20:21 +0000

    Courses.fetch_data() left align.

--- app/models/courses.py
+++ app/models/courses.py
@@ -22,7 +22,7 @@ class Courses(Collection):
         for course_id, course_name in self.collection.items():
             # Print out the course id and name as a heading title
             heading = f"{course_id} - {course_name.upper()}"
-            print(f"\n\033[45m[{heading:^85}]\033[0m")
+            print(f"\n\033[45m[{heading:<85}]\033[0m")
 
             # Start to fetch the course data
             a_course = Course(id=course_id)
