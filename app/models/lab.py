72a624a888ef4be0934433554726ab4ac22cdd4e
samdx <18287802+samdx@users.noreply.github.com>
2025-04-30 08:11:12 +0800

    Lab.save_markdown()

--- app/models/lab.py
+++ app/models/lab.py
@@ -37,18 +37,17 @@ class Lab(BaseEntity):
     # Save the Lab data to a Markdown file
     def save_markdown(self):
         """
-        Write the {self.to_dict()} into a Markdown files for each path.
+        Save the Lab data to a Markdown file.
         """
-        # Until we extract the lab content, for time being,
-        # can use the same helper for Path
-        md_helper = MDHelper()
-        # Generate the markdown content
-        path_md = md_helper.md_helper_lab(self.to_dict())
+
+        lab_md = self.generate_markdown()
 
         # Create the folder if it doesn't exist
         if not self._md_path.parent.exists():
             self._md_path.parent.mkdir(parents=True, exist_ok=True)
 
         # Write the markdown content to a file, overwrite if exists
-        with open(self._md_path, "w", encoding="utf-8", newline='\n') as md_file:
-            md_file.write(path_md)
+        with open(self._md_path, "w", encoding="utf-8", newline='\n') as mdfile:
+            mdfile.write(lab_md)
+
+        print(f"(Lab.save_markdown) Markdown file saved: {self._md_path}")
