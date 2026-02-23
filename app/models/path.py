66d44379a3155f634ec16b0c07e27d9d115bcb91
Sam Dinh <18287802+samdx@users.noreply.github.com>
2026-02-23 22:20:39 +0800

    fix: replaced * with - for markdown list

--- app/models/path.py
+++ app/models/path.py
@@ -126,7 +126,7 @@ class Path(BaseEntity):
             course_list = []
             for course_id, course in self.courses.items():
                 course_md_name = f"{util_replace_special_chars(course['name'])}.md"
-                course_list.append(f"* [ ] [{course['name']} ({course_id})](../courses/{course_md_name})")
+                course_list.append(f"- [ ] [{course['name']} ({course_id})](../courses/{course_md_name})")
             markdown.append("\n".join(course_list))
 
         return "\n\n".join(markdown) + "\n"
